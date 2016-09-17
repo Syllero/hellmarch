5237b2d71002c7bfdc3072651931e35593338626
Bentebent <henriksson.tim@gmail.com>
2016-09-17 18:12:44 +0200

    pusher unit script

---
+++ hellmarch/Assets/scripts/pusherUnit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pusherUnit : unit {
+
+	// Use this for initialization
+	new void Start () {
+        base.Start();
+        m_animator.SetBool("run", true);
+        m_movementSpeed = 20;
+    }
+	
+	// Update is called once per frame
+	new void Update () {
+        base.Update();
+	
+	}
+}
