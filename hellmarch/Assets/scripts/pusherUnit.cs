3d4cfd512f735738404ee2b6929bfcf533cd35e9
Bentebent <henriksson.tim@gmail.com>
2016-09-17 19:48:38 +0200

    pushing nuke

--- hellmarch/Assets/scripts/pusherUnit.cs
+++ hellmarch/Assets/scripts/pusherUnit.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class pusherUnit : unit {
 
     int pushRange = 10;
+    bool pushing = false;
 
 	// Use this for initialization
 	new void Start () {
@@ -24,14 +25,33 @@ public class pusherUnit : unit {
             {
                 Debug.Log(dist);
                 transform.parent = m_main.nuke.transform;
+
+                if (!pushing)
+                {
+                    m_main.nuke.GetComponent<nuke>().AddPusher(m_team);
+                    pushing = true;
+                }
             }
             else
             {
                 float step = m_movementSpeed * Time.deltaTime;
                 transform.position = Vector3.MoveTowards(transform.position, m_main.nuke.transform.position, step);
                 transform.LookAt(m_main.nuke.transform.position);
+                pushing = false;
             }
         }
 	
 	}
+
+    public override bool ReceiveDamage(int damage)
+    {
+        if(base.ReceiveDamage(damage))
+        {
+            m_main.nuke.GetComponent<nuke>().RemovePusher(m_team);
+            pushing = false;
+            return true;
+        }
+
+        return false;
+    }
 }
