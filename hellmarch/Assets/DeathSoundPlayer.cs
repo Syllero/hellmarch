fbe325bf7413ee54453b2442d50ab7b7eb246565
Bentebent <henriksson.tim@gmail.com>
2016-09-18 13:54:30 +0200

    Fixed death sound

--- hellmarch/Assets/DeathSoundPlayer.cs
+++ hellmarch/Assets/DeathSoundPlayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DeathSoundPlayer : MonoBehaviour {
 
 	private AudioSource[] sources;
+    bool isPlaying = false;
 	// Use this for initialization
 	void Start () {
 		sources = GetComponents<AudioSource> ();
@@ -15,6 +16,10 @@ public class DeathSoundPlayer : MonoBehaviour {
 	}
 
 	public void Play() {
-		sources [Random.Range (0, sources.Length)].Play ();
-	}
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            sources[Random.Range(0, sources.Length)].Play();
+        }
+    }
 }
