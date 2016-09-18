92a62a2f5d33380475c37aa312dc1c2c2c2cf14c
joelsven <josv.svensson@gmail.com>
2016-09-18 13:19:16 +0200

    Cloud rendering better shadows and light tweaks

---
+++ hellmarch/Assets/CloudsMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsMove : MonoBehaviour {
+
+	private float xpos = 0f;
+	private float zpos = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		xpos += Time.deltaTime * 6.0f;
+		if (xpos > 1000.0f) xpos -= 2000.0f;
+
+		zpos += Time.deltaTime * 10.0f;
+		if (zpos > 1000.0f) zpos -= 2000.0f;
+
+		transform.position = new Vector3(xpos, transform.position.y, zpos);
+	}
+}
